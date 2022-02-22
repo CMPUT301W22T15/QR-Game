d13fd4b75208367686f5eba03fa76105af35f5ee
Navdeep Singh <navdeep5@ualberta.ca>
2022-02-22 11:22:54 -0700

    Added ability to open up camera and scan

--- QRGameTeam15/app/src/main/java/com/example/qrgameteam15/MainActivity.java
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.qrgameteam15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // This button is just here for testing, it should later be moved to User Menu
+        Button scanButton = (Button) findViewById(R.id.scan);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ScannerView.class));
+            }
+        });
     }
+
+
+
 }
---
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/ScannerView.java
@@ -0,0 +1,104 @@
+package com.example.qrgameteam15;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.budiyev.android.codescanner.CodeScanner;
+import com.budiyev.android.codescanner.CodeScannerView;
+import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.zxing.Result;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+/**
+ * This class is responsible for the interface that allows Users to scan codes
+ * Ensures that users grant camera permission prior to use
+ */
+
+// The concept of this class was obtained from the following video:
+    // Video By: SmallAcademy
+    // Date: Nov. 10, 2019
+    // URL: https://youtu.be/Iuj4CuWjYF8
+public class ScannerView extends AppCompatActivity {
+    // Initalize variables
+    CodeScanner codeScanner;
+    CodeScannerView scannerView;
+    TextView resultData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scanner_view);
+
+        // Set variable data
+        scannerView = findViewById(R.id.scanner_view);
+        codeScanner = new CodeScanner(this, scannerView);
+
+        // Decode data from QR code
+        codeScanner.setDecodeCallback(new DecodeCallback() {
+            @Override
+            public void onDecoded(@NonNull Result result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        resultData.setText(result.getText());
+                    }
+                });
+            }
+        });
+
+        // Restart scan if UI is clicked
+        scannerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                codeScanner.startPreview();
+
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        requestForCamera();
+    }
+
+    /**
+     * This method asks for permission to use camera
+     * If accepted, then the Camera will open and scan
+     * If denied, a message will appear saying that permission is still needed
+     * Otherwise, it continues to display the message
+     */
+    private void requestForCamera(){
+        Dexter.withActivity(this).withPermission(Manifest.permission.CAMERA).withListener(new PermissionListener() {
+            @Override
+            public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                codeScanner.startPreview();
+            }
+
+            @Override
+            public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                Toast.makeText(ScannerView.this, "Camera Permission is Required", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                permissionToken.continuePermissionRequest();
+
+            }
+        }).check();
+    }
+}
+
