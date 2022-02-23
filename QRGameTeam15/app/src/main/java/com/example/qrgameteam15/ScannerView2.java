8c44bc9708bdc4ea95f0108b90eef6e3100bd778
Navdeep Singh <navdeep5@ualberta.ca>
2022-02-22 19:26:36 -0700

    Test Pull Request

--- QRGameTeam15/app/src/main/java/com/example/qrgameteam15/ScannerView2.java
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/ScannerView2.java
@@ -61,6 +61,7 @@ public class ScannerView2 extends AppCompatActivity implements ZXingScannerView.
         super.onPause();
         scannerView.stopCamera();
     }
+    
 
     @Override
     protected void onResume() {
