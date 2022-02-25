ab92e4d7c9f8cc2f47b80feda3f77ddacf799acd
truong <truonggi@ualberta.ca>
2022-02-25 11:57:12 -0700

    Added classes and some skeleton implementation

--- QRGameTeam15/app/src/main/java/com/example/qrgameteam15/Player.java
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/Player.java
@@ -1,5 +1,14 @@
 package com.example.qrgameteam15;
 
-public class Player {
+import java.util.ArrayList;
 
+public class Player {
+    private String username;
+    private ID id;
+    private PlayerScore playerScore;
+    public static ArrayList<QRcode2> QRcodes = new ArrayList<>();
+    public Player(String username) {
+        this.username = username;
+        this.id = new ID(username);
+    }
 }
---
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/PlayerScore.java
@@ -0,0 +1,24 @@
+package com.example.qrgameteam15;
+
+import java.util.ArrayList;
+
+public class PlayerScore {
+    private int totalScore;
+    private int maxScore;
+    private ArrayList<Integer> scores;
+
+    public PlayerScore() {
+        this.totalScore = 0;
+        this.maxScore = 0;
+        scores = new ArrayList<>();
+    }
+
+    public void addScore(int moreScore) {
+         // add to arralist
+    }
+    public int getMaxScore() {
+        // iterate and get max
+        // update this.maxscore
+        return 0;
+    }
+}
