6580a84cfc179e7f81682852131f79eb80eb189e
“Em” <98789620+em1i@users.noreply.github.com>
2022-03-08 16:09:47 -0700

    updated TakePhoto.java and activity_take_photo.xml with take photo code, added UploadPhoto class for TakePhoto.java use

--- QRGameTeam15/app/src/main/java/com/example/qrgameteam15/TakePhoto.java
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/TakePhoto.java
@@ -1,132 +1,255 @@
 package com.example.qrgameteam15;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.core.content.PackageManagerCompat;
+
+import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Environment;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.CollectionReference;
-import com.google.firebase.firestore.EventListener;
-import com.google.firebase.firestore.FieldValue;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreException;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import java.io.File;
+import java.io.IOException;
+import java.nio.channels.Channel;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TakePhoto extends AppCompatActivity {
-    // All code seen here that relates to ScannerView was added for purposes of testing and may later be removed
 
-    ListView playerList;
-    ArrayAdapter<Player> playerAdapter;
-    ArrayList<Player> playerDataList;
+    //updated by emily with take photo code (START)
 
-    CustomList customList;
+    public static final int CAMERA_PERM_CODE = 101;
+    public static final int CAMERA_REQUEST_CODE = 102;
+    private static final int PICK_IMAGE_REQUEST = 1;
 
-    final String TAG = "Player";
-    Button addPlayerButton;
-    static TextView playerName;
-    EditText addUserNameText;
-    EditText addScore;
-    FirebaseFirestore db;
+    ImageView selectedImage;
+    Button captureButton;
+    Button uploadButton;
+    Uri imageUri;
 
+    private StorageReference storageReference;
+    private DatabaseReference databaseReference;
+
+    String currentPhotoPath;
+
+    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult activityResult) {
+                    int result = activityResult.getResultCode();
+                    Intent data = activityResult.getData();
+
+                    //do something
+                    if(result == RESULT_OK) {
+
+//                        Bitmap image = (Bitmap) data.getExtras().get("data");
+//                        selectedImage.setImageBitmap(image);
+
+                        Toast.makeText(TakePhoto.this, "Use captured image. " + currentPhotoPath, Toast.LENGTH_LONG).show();
+
+                        File f = new File(currentPhotoPath);
+                        //display the image in the ImageView
+                        selectedImage.setImageURI(Uri.fromFile(f));
+
+                        //adding image to gallery
+//                        galleryAddPic();
+
+//                        uploadFile(f.getName(), Uri.fromFile(f));
+
+                    }
+                    else {
+                        Toast.makeText(TakePhoto.this, "Cancelled", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+    );
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_photo);
 
-        // This button is just here for testing, it should later be moved to User Menu
-        Button scanButton = (Button) findViewById(R.id.scan);
-        // moved it here
+        selectedImage = findViewById(R.id.imageView_displayCaptureImage);
+        captureButton = findViewById(R.id.btn_TakePhoto);
+        uploadButton = findViewById(R.id.btn_UploadPhoto);
 
+        storageReference = FirebaseStorage.getInstance().getReference("uploads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("uploads");
 
-        scanButton.setOnClickListener(new View.OnClickListener() {
+        captureButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), ScannerView.class));
+                //TEST-let user know the button works
+                Toast.makeText(TakePhoto.this, "Take Photo button pressed.", Toast.LENGTH_SHORT).show();
+
+                askCameraPermissions();
             }
         });
 
-        // Once clicked, the button will update the database
-        addPlayerButton = findViewById(R.id.addPlayer);
-        addUserNameText = findViewById(R.id.addUserName);
-        addScore = findViewById(R.id.score);
-
-        playerList = findViewById(R.id.player_list);
-        playerDataList = new ArrayList<>();
-        playerAdapter = new CustomList(this, playerDataList);
-        playerList.setAdapter(playerAdapter);
-
-        // Access a Cloud FireStore instance from Activity
-        db = FirebaseFirestore.getInstance();
-        final CollectionReference collectionReference = db.collection("Players");
-
-        addPlayerButton.setOnClickListener(new View.OnClickListener() {
+        uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                final String userName = addUserNameText.getText().toString();
-                final String score = addScore.getText().toString();
-
-                HashMap<String,String> data = new HashMap<>();
-                HashMap<String, Object>jsonData = new HashMap<>();
-
-                data.put("score", score);
-                //collectionReference.document(userName).update("scannedcodes", FieldValue.arrayUnion(score));
-                collectionReference
-                        .document(userName)
-                        .set(data)
-                        .addOnSuccessListener(new OnSuccessListener<Void>() {
-                            @Override
-                            public void onSuccess(Void unused) {
-                                Log.d(TAG,"Data has been added successfully");
-                            }
-                        })
-                        .addOnFailureListener(new OnFailureListener() {
-                            @Override
-                            public void onFailure(@NonNull Exception e) {
-                                Log.d(TAG,"Data could not be added!" + e.toString());
-                            }
-                        });
-                // add scannedcodes: ["array", "nano"]
-                collectionReference.document(userName).update("scannedcodes", FieldValue.arrayUnion("array"));
-                collectionReference.document(userName).update("scannedcodes", FieldValue.arrayUnion("nano"));
-                addUserNameText.setText("");
-                addScore.setText("");
-            }
-        });
-        collectionReference.addSnapshotListener(new EventListener<QuerySnapshot>() {
-            @Override
-            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException error) {
-                //Clear the old list
-                playerDataList.clear();
-                for (QueryDocumentSnapshot doc: queryDocumentSnapshots){
-                    Log.d(TAG, String.valueOf(doc.getData().get("score")));
-                    String user = doc.getId();
-                    String score = (String) doc.getData().get("score");
-                    playerDataList.add(new Player(user,score));
-                    // ACCESS the "scannedcoes" array element
-                    List<String> scannedCOdes = (List<String>) (doc.getData().get("scannedcodes"));
-                    for (int i = 0; i < scannedCOdes.size(); i++) {
-                        Log.d(TAG, scannedCOdes.get(i));
-                    }
-                }
-                playerAdapter.notifyDataSetChanged();
+                File f = new File(currentPhotoPath);
+                uploadFile(f.getName(), Uri.fromFile(f));
+//                Toast.makeText(MainActivity.this, "Upload: " + f.getName(), Toast.LENGTH_LONG).show();
             }
         });
     }
+
+
+    //check for user permission to use the camera
+    private void askCameraPermissions() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
+        } else{
+//            openCamera();
+            dispatchTakePictureIntent();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults); //is it needed????
+
+        if(requestCode == CAMERA_PERM_CODE) {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                openCamera();
+                dispatchTakePictureIntent();
+            }
+            else{
+                Toast.makeText(this, "Camera permission is required to use camera.", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void openCamera() {
+//        Toast.makeText(this, "Camera open request", Toast.LENGTH_SHORT).show();
+
+        //whenever this is called, it'll open the camera
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+//        startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+        activityResultLauncher.launch(cameraIntent);
+    }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(contentResolver.getType(uri));
+    }
+
+    private void uploadFile(String name, Uri uri){
+        if(uri != null){
+            //give it a unique file name
+            StorageReference fileReference = storageReference.child(name);
+
+            fileReference.putFile(uri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(TakePhoto.this, "Upload successful.", Toast.LENGTH_SHORT).show();
+
+//                            String uploadId = databaseReference.push().getKey();
+//                            databaseReference.child(uploadId).setValue(uri);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(TakePhoto.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+//                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+//                        @Override
+//                        public void onProgress(@NonNull UploadTask.TaskSnapshot snapshot) {
+//                            //update progress bar
+//                            //not needed for now
+//                        }
+//                    });
+        }
+        else{
+            Toast.makeText(this,"No file selected.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+//        String imageFileName = "JPEG_" + timeStamp + "_";
+        String imageFileName = timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+//                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                activityResultLauncher.launch(takePictureIntent);
+
+
+            }
+        }
+    }
+
+    private void galleryAddPic() {
+        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        File f = new File(currentPhotoPath);
+        Uri contentUri = Uri.fromFile(f);
+        mediaScanIntent.setData(contentUri);
+        this.sendBroadcast(mediaScanIntent);
+    }
+
 }
