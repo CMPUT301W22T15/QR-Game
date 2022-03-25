eec69830c1de4425fc82a6b2bdc7b111e79bb542
truong <truonggi@ualberta.ca>
2022-03-25 12:50:47 -0600

    now geolocation works on my emulator

--- QRGameTeam15/app/src/main/java/com/example/qrgameteam15/QRCodeEditor.java
+++ QRGameTeam15/app/src/main/java/com/example/qrgameteam15/QRCodeEditor.java
@@ -12,6 +12,7 @@ import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.Parcelable;
 import android.util.Log;
 import android.view.View;
@@ -24,6 +25,8 @@ import android.widget.Toast;
 
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -31,6 +34,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.android.gms.location.LocationRequest;
+
 
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -62,6 +67,8 @@ public class QRCodeEditor extends AppCompatActivity {
     FusedLocationProviderClient fusedLocationProviderClient;
     FirebaseFirestore db;
     CollectionReference collectionReference;
+    LocationCallback locationCallback;
+    LocationRequest locationRequest;
 
     private QRCode QR;
 
@@ -73,7 +80,8 @@ public class QRCodeEditor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrcode_editor);
-
+        db = FirebaseFirestore.getInstance();
+        collectionReference = db.collection("Players");
         // Set variable data
         newScan = findViewById(R.id.new_scan);
         score = findViewById(R.id.score);
@@ -83,10 +91,64 @@ public class QRCodeEditor extends AppCompatActivity {
         commentSection = findViewById(R.id.comments);
         commentInput = findViewById(R.id.comment_editor);
         postComment = findViewById(R.id.submit_comment);
-        db = FirebaseFirestore.getInstance();
-        // Initialize fusedLocationProviderClient
+
+
+        // Initialize fusedLocationProviderClient--------------------------------------------------
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-        collectionReference = db.collection("Players");
+        // TODO: set up location request
+        locationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);  // get location .setInterval(5) to
+        // callback for we get the location back from fused location service
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                // here is the location
+                if (locationResult == null) {
+                    Toast.makeText(QRCodeEditor.this, "null location", Toast.LENGTH_SHORT).show();
+                    fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+                    return;
+                }
+                Location lastLocation = locationResult.getLastLocation();
+                // do what needs to be done
+                double lat = lastLocation.getLatitude();
+                double lon = lastLocation.getLongitude();
+                //Log.i("TAG", "inCallBack" + " " + lat + " " + lon);
+                String latitudeString = Double.toString(lat);
+                String longitudeString = Double.toString(lon);
+                int lengthQRCode = singletonPlayer.player.qrCodes.size();
+                String locationString = latitudeString+" "+longitudeString;  //TODO changed "-" to ""
+                QRCode qrCode = singletonPlayer.player.qrCodes.get(lengthQRCode-1);
+                // [0, 1, 2]
+                qrCode.idObject.setLocationStr(locationString);
+                String hashLoc = qrCode.getSha256Hex();
+                qrCode.idObject.setHashedID(hashLoc +"-"+ locationString);
+                qrCode.setLocation(locationString);
+                qrCode.hasLocation = true;
+                singletonPlayer.player.qrCodes.set(lengthQRCode-1, qrCode);
+                String TAG = "working";
+                Toast.makeText(QRCodeEditor.this, "saved geolocation", Toast.LENGTH_SHORT).show();
+                collectionReference
+                        .document(singletonPlayer.player.getUsername())
+                        .set(singletonPlayer.player)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+                                Log.d(TAG,"message");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.e("MYAPP", "exception: " + e.getMessage());
+                                Log.e("MYAPP", "exception: " + e.toString());
+                            }
+                        });
+                // deactivate callback
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback);
+            }
+        };
+        // ----------------------------------------------------------------------------------
+
 
 
         // Get intent
@@ -122,7 +184,8 @@ public class QRCodeEditor extends AppCompatActivity {
                         Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
                         && ActivityCompat.checkSelfPermission(QRCodeEditor.this,
                         Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    getLocation();
+                    //getLocation();
+                    getLocation1();
                 } else {
                     ActivityCompat.requestPermissions(QRCodeEditor.this,
                             new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 44);
@@ -137,6 +200,9 @@ public class QRCodeEditor extends AppCompatActivity {
 
         }
 
+    /**
+     * outdated version that dont work on emulator
+     */
     @SuppressLint("MissingPermission")
     private void getLocation() {
         fusedLocationProviderClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
@@ -187,6 +253,17 @@ public class QRCodeEditor extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    /**
+     * get the location and go to the callback when done
+     */
+    @SuppressLint("MissingPermission")
+    public void getLocation1() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        //client.requestLocationUpdates(locationRequest, callback, Looper.getMainLooper());
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
     }
 
     /**
