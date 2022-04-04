6dbbfad3cdb9463519d7ca648f55a8318993e4c4
truong <truonggi@ualberta.ca>
2022-04-04 10:20:26 -0600

    scan qrcode test done

--- QRGameTeam15/app/src/androidTest/java/com/example/qrgameteam15/MainNavigationTest.java
+++ QRGameTeam15/app/src/androidTest/java/com/example/qrgameteam15/MainNavigationTest.java
@@ -1,6 +1,7 @@
 package com.example.qrgameteam15;
 
 import android.app.Activity;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -50,7 +51,7 @@ public class MainNavigationTest {
         // create a random user each time
         solo.assertCurrentActivity("Wrong Activity", NewUser.class);
         Random random = new Random();
-        int randomint = random.nextInt(100 - 0) + 0;
+        int randomint = random.nextInt(10000 - 0) + 0;
         String enterUsername = "AndroidTestUser" + Integer.toString(randomint);
         solo.enterText((EditText) solo.getView(R.id.username_text), enterUsername);
         solo.enterText((EditText) solo.getView(R.id.name_text), "1");
@@ -63,10 +64,6 @@ public class MainNavigationTest {
         solo.sleep(2000); // 2 pause
         solo.assertCurrentActivity("Wrong Activity", UserMenu.class);
         // go to
-
-        
-
-
 
         // test logout functionaly
         solo.assertCurrentActivity("Wrong Activity", UserMenu.class);
@@ -84,6 +81,41 @@ public class MainNavigationTest {
     @Test
     public void scanAQRcode() {
         // click scannerView
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        solo.clickOnButton("New User"); //Click ADD CITY Button
+        solo.sleep(2000); // 2 pause
+
+        // create a random user each time
+        solo.assertCurrentActivity("Wrong Activity", NewUser.class);
+        Random random = new Random();
+        int randomint = random.nextInt(10000 - 0) + 0;
+        String enterUsername = "AndroidTestUser" + Integer.toString(randomint);
+        solo.enterText((EditText) solo.getView(R.id.username_text), enterUsername);
+        solo.enterText((EditText) solo.getView(R.id.name_text), "1");
+        solo.enterText((EditText) solo.getView(R.id.email_text), "1@gmail.com");
+        solo.enterText((EditText) solo.getView(R.id.city_region), "edmonton");
+        //solo.clickOnButton("CREATE ACCOUNT");  // Click ADD CITY Button
+
+        solo.clickOnView(solo.getView(R.id.create_userBtn));
+
+        solo.sleep(2000); // 2 pause
+        solo.assertCurrentActivity("Wrong Activity", UserMenu.class);
+
+        // scan a code
+        solo.clickInList(2); // click scan new code
+        solo.sleep(10000); // 10s  pause to let user/emu scan a code
+        solo.assertCurrentActivity("Wrong Activity", QRCodeEditor.class);  // assert we in editor
+
+        // get the score textview
+        String scoreTextV = solo.getText(1).getText().toString();
+
+        solo.clickOnView(solo.getView(R.id.save));  // go back to main menu
+        solo.sleep(2000);
+        solo.assertCurrentActivity("Wrong Activity", UserMenu.class);  // assert we in menu
+
+        solo.clickInList(3); // click myscan
+        solo.sleep(2000);
+        solo.assertCurrentActivity("Wrong Activity", MyScans.class);  // assert we in menu
     }
 
 
